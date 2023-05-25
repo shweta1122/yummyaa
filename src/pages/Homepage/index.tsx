e3bce01f3ff850cf836ef998e2b7391c24c53f77
sweta chaurasia <shweta.chaurasia98@gmail.com>
2023-05-25 23:19:35 +0530

    left para

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -4,6 +4,15 @@ const HomePage: React.FC<{}> = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit
+        </p>
+      </div>
     </div>
   );
 };
