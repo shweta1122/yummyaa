9d17fdb2be8549a76bbd9fcdf7e52a82493bf5fe
sweta chaurasia <shweta.chaurasia98@gmail.com>
2023-05-26 23:35:00 +0530

    Title changes

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -4,13 +4,26 @@ const HomePage: React.FC<{}> = () => {
   return (
     <div>
       <Navbar />
-      <div>
+      <div className="main-body1">
+        <div className="main-body-title">
+          <h3>
+            Food that makes you say wow,<br></br> we welcome here all the
+            <br></br>
+            foodies!!!!
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex
+          </p>
+        </div>
+
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit
+          aliquip ex
         </p>
       </div>
     </div>
